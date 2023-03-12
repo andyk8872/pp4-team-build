86fda35e2240a0be449121e4599257f3a13b5510
andyk8872 <andrewkennedy35@yahoo.ie>
2023-03-12 14:32:08 +0000

    add html/css to index/home page

---
+++ static/js/script.js
@@ -0,0 +1,15 @@
+// copyright
+document.getElementById("copyright").textContent = (new Date().getFullYear());
+
+// map
+var map = L.map('map').setView([54.27, -8.4961], 14);
+
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([54.2673456244, -8.4723157056]).addTo(map);
+
+marker.bindPopup("<b>TEAM | BUILD</b>").openPopup();
