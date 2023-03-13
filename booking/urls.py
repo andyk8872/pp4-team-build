35b25f96f7f116a8d0c845394824db87eacf9775
andyk8872 <andrewkennedy35@yahoo.ie>
2023-03-13 18:33:30 +0000

    fix: edit 'delete' in edit_booking

--- booking/urls.py
+++ booking/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("", views.HomePage.as_view(), name="home"),
     path("make_booking/", views.make_booking, name="make_booking"),
     path('my_account', views.view_booking, name='my_account'),
+    path('edit_booking/<booking_id>', views.edit_booking, name='edit_booking'),
+    path('delete/<booking_id>', views.delete_booking, name='delete'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from django.views.generic import TemplateView
 from .forms import BookingForm
@@ -19,7 +19,7 @@ def make_booking(request):
         form = BookingForm(request.POST, instance=booking)
         if form.is_valid():
             form.save()
-            return redirect('home')
+            return redirect('my_account')
     else:
         form = BookingForm()
     return render(request, 'make_booking.html', {'form': form})
@@ -27,9 +27,36 @@ def make_booking(request):
 
 @login_required
 def view_booking(request):
-   
+
     bookings = request.user.bookings.all()
     context = {
         'bookings': bookings,
     }
-    return render(request, 'my_account.html', context) 
+    return render(request, 'my_account.html', context)
+
+
+@login_required
+def edit_booking(request, booking_id):
+    try:
+        booking = get_object_or_404(Booking, id=booking_id)
+        if request.method == 'POST':
+            form = BookingForm(request.POST, instance=booking)
+            if form.is_valid():
+                form.save()
+                return redirect('my_account')
+        else:
+            form = BookingForm(instance=booking)
+        context = {
+                 'form': form
+        }
+        return render(request, 'edit_booking.html', context)
+
+    except Http404 as err:
+        return redirect('my_account')
+
+
+@login_required
+def delete_booking(request, booking_id):
+    booking = get_object_or_404(Booking, id=booking_id)
+    booking.delete()
+    return redirect('my_account')
