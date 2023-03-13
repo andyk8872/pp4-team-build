5cfad298fa3079f0c4f1ebfc8eb0100b18a9b92f
andyk8872 <andrewkennedy35@yahoo.ie>
2023-03-13 13:02:06 +0000

    feat: add make_booking in views.py

--- booking/views.py
+++ booking/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.views.generic import TemplateView
+from .forms import BookingForm
+from .models import Booking
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 
 class HomePage(TemplateView):
     template_name = "index.html"
+
+
+@login_required
+def make_booking(request):
+
+    if request.method == 'POST':
+        booking = Booking(user=request.user)
+        form = BookingForm(request.POST, instance=booking)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = BookingForm()
+    return render(request, 'make_booking.html', {'form': form})  
