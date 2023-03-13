27db55016914d5186a283b373b36ddc6b1e174f8
andyk8872 <andrewkennedy35@yahoo.ie>
2023-03-13 12:56:26 +0000

    feat: add forms.py for booking

---
+++ booking/forms.py
@@ -0,0 +1,30 @@
+from .models import Booking
+from django import forms
+import datetime
+
+
+class DateInput(forms.DateInput):
+
+    input_type = 'date'
+
+
+class BookingForm(forms.ModelForm):
+
+    class Meta:
+        model = Booking
+        fields = ('contact_name',
+                  'email',
+                  'total_no_group',
+                  'activity_type',
+                  'booking_date',)
+        widgets = {
+            'booking_date': DateInput(attrs={
+                'required': True,
+                'min': datetime.date.today()+datetime.timedelta(days=2),
+                'max': datetime.date.today()+datetime.timedelta(days=60)}),
+            'email': forms.EmailInput(attrs={
+                'required': True}),
+            'contact_name': forms.TextInput(attrs={
+                'required': True,
+            })
+                }
