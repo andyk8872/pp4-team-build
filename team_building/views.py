55ee1c0a02f7c897b6dc45b74b8658cb32dc4b8c
andyk8872 <andrewkennedy35@yahoo.ie>
2023-03-24 05:14:48 +0000

    feat: add docstrings

--- team_building/views.py
+++ team_building/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 
 def page_not_found_view(request, exception):
+    """
+    Creates an error page.
+    """
     return render(request, '404.html', status=404)
