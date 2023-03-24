b116e2502fa991856f8d0a581807f4e1b300c160
andyk8872 <andrewkennedy35@yahoo.ie>
2023-03-24 05:01:25 +0000

    feat: add docstrings

--- booking/admin.py
+++ booking/admin.py
@@ -4,6 +4,9 @@ from .models import Booking, Review
 
 @admin.register(Booking)
 class CommentAdmin(admin.ModelAdmin):
+    """
+    Displays the fields for the Booking model
+    """
     list_display = ('user', 'total_no_group', 'creation_date', 'approved')
     list_filter = ('approved', 'booking_date')
     search_fields = ('contact_name', 'booking_date')
@@ -11,5 +14,7 @@ class CommentAdmin(admin.ModelAdmin):
 
 @admin.register(Review)
 class Review(admin.ModelAdmin):
-
+    """
+    Displays the fields for the Review model
+    """
     list_display = ('user', 'creation_date', 'review', 'approved')
